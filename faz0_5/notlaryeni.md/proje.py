66d7e137905a6f7e251912fe35d041c984a362cb
aslı cebecioğlu <aslicebecioglu@192.168.1.96>
2026-07-23 00:59:03 +0300

    tamamlandı

---
+++ faz0_5/notlaryeni.md/proje.py
@@ -0,0 +1,14 @@
+try: 
+    yas=int("beş")
+except ValueError:
+    print("Sayı giriniz!")
+def kisi_olustur(isim, yas, sehir):
+    kisi = {"isim": isim, "yas": yas, "sehir": sehir}
+    return kisi
+kisi1=kisi_olustur("Aslı", 19, "İstanbul")
+kisi2=kisi_olustur("mehmet",20,"Ankara")
+kisi3=kisi_olustur("melike",7,"İstanbul")
+kisiler=[kisi1,kisi2,kisi3]
+for kisi1 in kisiler:
+    print(f"{kisi1['isim']} {kisi1['yas']} yaşında {kisi1['sehir']}'da yaşıyor.")
+
