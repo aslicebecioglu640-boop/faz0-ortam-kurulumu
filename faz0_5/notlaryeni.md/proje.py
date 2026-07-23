1d060e2e8d2b84991a70f7cfec8aee75641e734f
aslı cebecioğlu <aslicebecioglu@asl-MacBook-Air.local>
2026-07-23 12:03:04 +0300

    tamam.

--- faz0_5/notlaryeni.md/proje.py
+++ faz0_5/notlaryeni.md/proje.py
@@ -1,11 +1,14 @@
-try: 
-    yas=int("beş")
-except ValueError:
-    print("Sayı giriniz!")
+
+    
 def kisi_olustur(isim, yas, sehir):
+    try:
+        yas=int(yas)
+    except ValueError:
+        print("Sayı giriniz!")
+        yas=None
     kisi = {"isim": isim, "yas": yas, "sehir": sehir}
     return kisi
-kisi1=kisi_olustur("Aslı", 19, "İstanbul")
+kisi1=kisi_olustur("Aslı", "ondokuz", "İstanbul")
 kisi2=kisi_olustur("mehmet",20,"Ankara")
 kisi3=kisi_olustur("melike",7,"İstanbul")
 kisiler=[kisi1,kisi2,kisi3]
